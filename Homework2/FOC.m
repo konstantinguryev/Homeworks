4ee45827d5488d27a9c539c4a1b4e68a8fbdc057
Roman Istomin <rji5040@psu.edu>
2018-10-07 16:14:17 -0400

    you get check plus, but take a look at answer key. learn to use vector notarion in matlab, it saves you lots of space

--- Homework2/FOC.m
+++ Homework2/FOC.m
@@ -2,6 +2,9 @@ function [fval, Jac]=FOC(p,v)
 %Here we have fval = -Di(p)/D'i(p)-pi, i.e. the FOC of the profit function,
 % where i=A,B.
 
+% learn to write everything in vector form like in answer key. if you had
+% 100 products, your code would be a nightnare to read. matlab understands
+% if a math operation is vectorized. 
 fval=[(1+exp(v(1)-p(1))+exp(v(2)-p(2)))/(1+exp(v(2)-p(2)))-p(1);(1+exp(v(1)-p(1))+exp(v(2)-p(2)))/(1+exp(v(1)-p(1)))-p(2)];
 Jac=[(-exp(v(1)-p(1)))/(1+exp(v(2)-p(2)))-1 (exp(sum(v)-sum(p)))/(1+exp(v(2)-p(2)))^2;...
       (exp(sum(v)-sum(p)))/(1+exp(v(1)-p(1)))^2 (-exp(v(2)-p(2)))/(1+exp(v(1)-p(1)))-1];
