0fc5ba4c3513afed92f036591b66024a8b7160a7
Roman Istomin <rji5040@psu.edu>
2019-02-19 12:41:04 -0500

    you get check minus, check answer key

--- Homework7/HA_7_Guryev.m
+++ Homework7/HA_7_Guryev.m
@@ -4,7 +4,7 @@ V_0 = (repmat(v,L,L) + repmat(c',L,1))./(2*beta);
 P_0 = 1.5*repmat(c',L,1);
 
 [value, p, it] = VFI(V_0, P_0, stop, L, c, lambda, beta, v, tr_pr);
-
+% it did not converge to the equilibrium, see answer key
 figure(1);
 mesh(value); 
 title('Value Func');
